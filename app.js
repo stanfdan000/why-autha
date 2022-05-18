7245211b8cc61da940ef845864ea8f97de4a4ebc
sirdan <stanford303@gmail.com>
2022-05-18 09:50:23 -0700

    removed comment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
-//const signUpEmail = document.getElementById('sign-up-email');
+
 //const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
